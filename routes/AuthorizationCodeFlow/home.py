953ad056022342a59d56acd2e5e16e1f196b8c8f
yutadd <spyk4m49@gmail.com>
2024-06-05 16:29:23 +0900

    [refactoring]directory structure

--- routes/AuthorizationCodeFlow/home.py
+++ routes/AuthorizationCodeFlow/home.py
@@ -1,5 +1,5 @@
 from http.server import BaseHTTPRequestHandler
-from util.assembleResponse import redirectToLoginPage
+from util.assembleResponse import redirectToLoginPage, returnLoggedinContent
 from http.cookies import SimpleCookie
 
 def RedirectByState(context:BaseHTTPRequestHandler):
@@ -7,9 +7,5 @@ def RedirectByState(context:BaseHTTPRequestHandler):
         cookie = SimpleCookie(context.headers["Cookie"])
         if "session_id" in cookie:
             # ここでセッションIDの検証を行う（例：データベースで確認）
-            context.send_response(200)
-            context.send_header('Content-Type', 'text/html; charset=utf-8')
-            context.end_headers()
-            context.wfile.write(bytes("ログインしています", 'utf-8'))
-            return
+            returnLoggedinContent(context)
     redirectToLoginPage(context)
--- util/assembleResponse.py
+++ util/assembleResponse.py
@@ -7,7 +7,12 @@ def returnLoginUIToUA(context: BaseHTTPRequestHandler):
     context.send_header('Content-Type', 'text/html; charset=utf-8')
     context.end_headers()
     context.wfile.write(bytes(content, 'utf-8'))
-
+def returnLoggedinContent(context: BaseHTTPRequestHandler):
+    context.send_response(200)
+    context.send_header('Content-Type', 'text/html; charset=utf-8')
+    context.end_headers()
+    context.wfile.write(bytes("ログインしています", 'utf-8'))
+    return
 def returnErrorUIToUA(context: BaseHTTPRequestHandler,error:str,error_detail:str):
     with open('template/error.html', 'r', encoding='utf-8') as file:
         content = file.read()
