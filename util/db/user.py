57f7a5e4fb712d17ea2999340eccf951fbd1f9ea
yutadd <spyk4m49@gmail.com>
2024-06-11 19:40:59 +0900

    [update]start script

--- util/db/user.py
+++ util/db/user.py
@@ -43,7 +43,6 @@ def register_user(token: str, tokentype: str, token_expires_in: int,refresh_expi
     conn.commit()
     print("user registered")
 
-
 def get_user_by_sessionid(session_id: str) -> User:
     db_path = './db/users.db'
     os.makedirs(os.path.dirname(db_path), exist_ok=True)
@@ -52,6 +51,4 @@ def get_user_by_sessionid(session_id: str) -> User:
     cursor.execute("select id,token, token_type, expires_at, refresh_token,refresh_expires_at, scope, session_id,username from clients where session_id=?", (session_id,))
     user = cursor.fetchone()
     return User(id=user[0], token=user[1], token_type=user[2], expires_at=user[3], refresh_token=user[4],refresh_expires_at=user[5], scope=user[6], session_id=user[7],username=user[8])
-
-
 migrate_user_db()
