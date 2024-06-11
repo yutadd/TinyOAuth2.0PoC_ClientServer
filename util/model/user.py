aae03e4432f293e54e04903cd13c6c014cf4dd25
yutadd <spyk4m49@gmail.com>
2024-06-11 19:07:50 +0900

    [fix bugs]

--- util/model/user.py
+++ util/model/user.py
@@ -4,13 +4,17 @@ class User:
     token_type:str
     expires_at:str
     refresh_token:str
+    refresh_expires_at:str
     scope:str
+    username:str
     session_id:str
-    def __init__(self,id:int,token:str,token_type:str,expires_at:str,refresh_token:str,scope:str,session_id:str) -> None:
+    def __init__(self,id:int,token:str,token_type:str,expires_at:str,refresh_token:str,refresh_expires_at:str,scope:str,session_id:str,username:str) -> None:
         self.id=id
         self.token=token
         self.token_type=token_type
         self.expires_at=expires_at
         self.refresh_token=refresh_token
+        self.refresh_expires_at=refresh_expires_at
         self.scope=scope
         self.session_id=session_id
+        self.username=username
